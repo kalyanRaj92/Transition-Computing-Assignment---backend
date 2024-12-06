cdb561dca8082fe1a98f9d97f426f37bbf8691e9
kalyanRaj92 <burrikalyanraj@gmail.com>
2024-12-06 17:02:31 +0530

    Second commit

--- index.js
+++ index.js
@@ -1,38 +1,58 @@
-import express from 'express';
-import cors from 'cors';
-import axios from 'axios';
+import express from "express";
+import cors from "cors";
+import axios from "axios";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 app.use(cors());
+app.use(express.json());
 
+// Evaluate checklist for each application
 const evaluateChecklist = (data) => {
-    return{
-        valuationFeePaid: data.isValuationFeePaid === true,
-        ukResident: data.isUkResident === true,
-        riskRatingMedium: data.riskRating === "Medium",
-        ltvBelow60: (data.loanRequired / data.purchasePrice) * 100 < 60,
-    };
+  return data.map((each) => {
 
+    const valuationFeePaid = each.isValuationFeePaid === true;
+    const ukResident = each.isUkResident === true;
+    const riskRatingMedium = each.riskRating === "Medium";
+    const ltvBelow60 = (each.loanRequired / each.purchasePrice) * 100;
+
+    const status =
+      valuationFeePaid &&
+      ukResident &&
+      riskRatingMedium &&
+      ltvBelow60 < 60
+        ? "Passed"
+        : "Failed";
+
+    return {
+      applicationId: each.id,
+      valuationFeePaid,
+      ukResident,
+      riskRatingMedium,
+      ltvBelow60,
+      status,
+    };
+  });
 };
 
-const URL = "http://qa-gb.api.dynamatix.com:3100/api/applications/getApplicationById/67339ae56d5231c1a2c63639"
 
-app.get('/checklist',async(req, res)=>{
-    try {
-        const response = await axios.get(URL);
-        const data = response.data;
+const URL = "http://qa-gb.api.dynamatix.com:3100/api/applications";
+// API endpoint to fetch data
+app.get("/checklist", async (req, res) => {
+  try {
+    const response = await axios.get(URL);
+    const applications = response.data;
 
-        const results = evaluateChecklist(data);
-        res.status(200).json(results);
-    } catch (err) {
-        res.status(500).send({ error: 'Error fetching data', message: err.message });
-    }
-})
+    const results = evaluateChecklist(applications);
+    res.json(results);
+  } catch (err) {
+    res
+      .status(500)
+      .send({ error: "Error fetching data", message: err.message });
+  }
+});
 
-
-app.listen(PORT,()=>{
-    console.log(`Server is running on http://localhost:${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
